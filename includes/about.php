0adb4305760019f667dac9c6245f15e4c92afd76
Thamme Gowda <tgowdan@gmail.com>
2021-09-27 15:49:11 -0700

    update email admin2 -> host

--- includes/about.php
+++ includes/about.php
@@ -1,31 +1,27 @@
 
 <div class="px-8 py-2 text-center">
-    <h2 class="display-6">USC ISI NL Seminar</h2>
-    <div class="col-lg-7 mx-auto">
+    <h2 class="display-7">USC ISI NL Seminar</h2>
+    <div class="col-lg-7 mx-auto mb-0">
         <p class="lead">
-           A weekly meeting of the <a href="https://nlg.isi.edu">Natural Language Group</a>. <br/>
+           A weekly meeting of the <a href="https://nlg.isi.edu">Natural Language Group</a>.
             Seminars usually take place on <b>Thursdays from 11:00am until 12:00pm</b>.<br/>
         </p>
         <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <a type="button" target="_blank" href="https://mailman.isi.edu/mailman/listinfo/nlg-seminar" class="btn btn-outline-secondary btn-lg px-4 gap-3">Get Emails</a>
-            <a type="button" target="_blank" href="webcal://p51-caldav.icloud.com/published/2/MTk1OTQwNzY0ODE5NTk0MMoybo1JVpi4UvUZB6Mmydt-xNID2W6VMD_up6-qvFewPSLr8y-XoQXFsniXd4r6k5qmJKWByn_vY7eTuwRcY_A" class="btn btn-outline-secondary btn-lg px-4">Add to Calendar (iCal)</a>
+            <a type="button" target="_blank" href="https://mailman.isi.edu/mailman/listinfo/nlg-seminar" class="btn btn-outline-secondary btn-lg px-4 gap-3">Subscribe</i></a>
+            <a type="button" target="_blank" href="webcal://p51-caldav.icloud.com/published/2/MTk1OTQwNzY0ODE5NTk0MMoybo1JVpi4UvUZB6Mmydt-xNID2W6VMD_up6-qvFewPSLr8y-XoQXFsniXd4r6k5qmJKWByn_vY7eTuwRcY_A" class="btn btn-outline-secondary btn-lg px-4">Add to Calendar</a>
         </div>
-        <p class="lead"> For scheduling a talk, contact the current organizers:<br/>
-            <a href="https://isi.edu/~tg" target="_blank">Thamme Gowda</a> (<code>tg[at]isi.edu</code>)
-            or <a href="https://isi.edu/~jonmay" target="_blank"> Jon May</a> (<code>jonmay[at]isi.edu</code>). </p>
+        <p class="lead mt-3"> For scheduling a talk scheduling, contact the current organizer <a href="https://isi.edu/~tg" target="_blank">Thamme Gowda</a>, and <a href="https://isi.edu/~jonmay" target="_blank"> Jon May</a> by emailing to <code> nlg-seminar-host(at)isi.edu</code> .</p>
     </div>
 </div>
 
-<div class="seminar_about" style="margin-left:10px; 1ine-height:1.5em;">
-    <div>
+<div class="display-7">
         <a class="text-decoration-none" href="visitors.php"><h5>Visitor information</h5></a>
         <p><b>Virtual visit:</b>
             Meeting hosts only admit guests that they know to the Zoom meeting.
-            Hence, <b>you're highly encouraged to use your <i>USC account</i> to sign into Zoom.</b> <br/>
-            If you're an outside visitor, please inform us at (<code>nlg-seminar-admin2 [at] isi.edu</code>) beforehand
+            Hence, you're highly encouraged to use your <a href="https://itservices.usc.edu/zoom/">USC account to sign into Zoom</a>.<br/>
+            If you're an outside visitor, please inform us at (<code style="font-size: 1.1em">nlg-seminar-host(at)isi.edu</code>) beforehand
             so we'll be aware of your attendance and let you in.</p>
         <p><b>In-person visit:</b> Outside visitors should go to the tenth floor lobby where they will be met and
             escorted to the appropriate location <i>5 minutes before</i> the talk.</p>
-    </div>
     <p>Click on the titles to view the abstracts/time/location/online meeting link.</p>
 </div>
